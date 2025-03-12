3ff3ce764764dd2ac680cbe5a4f94847dc3d9a06
felipecartaxo <felipecartaxo1@gmail.com>
2025-03-12 20:17:44 -0300

    criando component tasks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule,
