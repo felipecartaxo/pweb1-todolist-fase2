2516a4ee8987a9740de07849b7d09a42f9fec86c
felipecartaxo <felipecartaxo1@gmail.com>
2025-03-17 18:43:46 -0300

    conexão com o backend em Spring

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -12,67 +12,98 @@ export class TasksComponent implements OnInit {
 
   // Variável que vai armazenar a lista de tarefas
   tarefas: Tarefa[] = [];
-   // Filtro de título
-  filtro: string = '';
-  // Filtro de categoria
-  filtroCategoria: string = '';
 
   constructor(private taskService: TaskService) {}
 
   // Após o construtor, o ngOnInit será chamado e irá listar todas as tarefas
   ngOnInit() {
-    this.carregarTarefas();
-  }
 
-  carregarTarefas() {
-    this.taskService.getTasks().subscribe((dados: Tarefa[]) => {
+    this.taskService.getTasks().subscribe((dado) => {
       // Armazena a lista de tarefas que foi retornada pelo serviço getTasks
-      this.tarefas = dados;
+      this.tarefas = dado;
 
       // Apenas para fins de teste
-      console.log(dados);
+      console.log(dado);
     });
   }
 
   addTask(tarefa: Tarefa) {
-    this.taskService.addTask(tarefa).subscribe(() => {
+    this.taskService.addTask(tarefa).subscribe((tarefa) => {
       // Lógica para listar a tarefa logo após criá-la
-      this.carregarTarefas();
+      this.tarefas.push(tarefa);
     });
   }
 
   toggleConcluido(tarefa: Tarefa) {
     tarefa.concluido = !tarefa.concluido;
-    this.taskService.updateTask(tarefa).subscribe(() => {
-      this.carregarTarefas();
-    });
+    this.taskService.updateTask(tarefa).subscribe();
   }
 
-  deleteTask(tarefa: Tarefa) {
-    if (tarefa.id) {
-      this.taskService.deleteTask(tarefa.id).subscribe(() => {
-        this.carregarTarefas();
-      });
-    }
+  deleteTask(tarefa: Tarefa){
+    this.taskService.deleteTask(tarefa).subscribe(() =>
+      (this.tarefas = this.tarefas.filter((t) => t.id !== tarefa.id)));
   }
 
-   /* buscarTarefas() {
-      if (this.filtro.trim()) {
-        this.taskService.pesquisarPorFiltro(this.filtro).subscribe((dados: Tarefa[]) => {
-          this.tarefas = dados;
-        });
-      } else {
-        this.carregarTarefas(); // Recarrega todas as tarefas se o filtro estiver vazio
-      }
-    } */
-    buscarTarefas() {
-      this.taskService.pesquisarPorFiltro(this.filtro, this.filtroCategoria).subscribe((dados: Tarefa[]) => {
-        this.tarefas = dados;
-      });
-    }
-
+  // TODO: Comentei apenas para testar a conexão com o Spring
+  // // Variável que vai armazenar a lista de tarefas
+  // tarefas: Tarefa[] = [];
+  //  // Filtro de título
+  // filtro: string = '';
+  // // Filtro de categoria
+  // filtroCategoria: string = '';
+  //
+  // constructor(private taskService: TaskService) {}
+  //
+  // // Após o construtor, o ngOnInit será chamado e irá listar todas as tarefas
+  // ngOnInit() {
+  //   this.carregarTarefas();
+  // }
+  //
+  // carregarTarefas() {
+  //   this.taskService.getTasks().subscribe((dados: Tarefa[]) => {
+  //     // Armazena a lista de tarefas que foi retornada pelo serviço getTasks
+  //     this.tarefas = dados;
+  //
+  //     // Apenas para fins de teste
+  //     console.log(dados);
+  //   });
+  // }
+  //
+  // addTask(tarefa: Tarefa) {
+  //   this.taskService.addTask(tarefa).subscribe(() => {
+  //     // Lógica para listar a tarefa logo após criá-la
+  //     this.carregarTarefas();
+  //   });
+  // }
+  //
+  // toggleConcluido(tarefa: Tarefa) {
+  //   tarefa.concluido = !tarefa.concluido;
+  //   this.taskService.updateTask(tarefa).subscribe(() => {
+  //     this.carregarTarefas();
+  //   });
+  // }
+  //
+  // deleteTask(tarefa: Tarefa) {
+  //   if (tarefa.id) {
+  //     this.taskService.deleteTask(tarefa.id).subscribe(() => {
+  //       this.carregarTarefas();
+  //     });
+  //   }
+  // }
+  //
+  //  /* buscarTarefas() {
+  //     if (this.filtro.trim()) {
+  //       this.taskService.pesquisarPorFiltro(this.filtro).subscribe((dados: Tarefa[]) => {
+  //         this.tarefas = dados;
+  //       });
+  //     } else {
+  //       this.carregarTarefas(); // Recarrega todas as tarefas se o filtro estiver vazio
+  //     }
+  //   } */
+  //
+  //   buscarTarefas() {
+  //     this.taskService.pesquisarPorFiltro(this.filtro, this.filtroCategoria).subscribe((dados: Tarefa[]) => {
+  //       this.tarefas = dados;
+  //     });
+  //   }
 }
-
-
-
-
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -3,96 +3,112 @@ import { AngularFirestore, AngularFirestoreCollection, DocumentReference } from 
 import { from, Observable, map, switchMap } from 'rxjs';
 import { Tarefa } from '../model/Tarefa';
 import { UsuarioService } from './usuario.service';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TaskService {
-  private injetor = inject(Injector);
-  private colecaoTarefas: AngularFirestoreCollection<Tarefa>;
-  private NOME_COLECAO = 'tarefas';
 
-  constructor(private firestore: AngularFirestore, private usuarioService: UsuarioService) {
-    this.colecaoTarefas = this.firestore.collection(this.NOME_COLECAO);
-    runInInjectionContext(this.injetor, () => {
-      this.colecaoTarefas = this.firestore.collection(this.NOME_COLECAO);
-    });
-  }
+// ----- SPRING BOOT -----
+export class TaskService {
+
+  // private URL = 'http://localhost:3000/tasks'; // URL do json server
+  private URL = 'http://localhost:8080/tarefas';
+
+  constructor(private http: HttpClient) { }
 
   // Listagem das tarefas
-  getTasks(): Observable<Tarefa[]> {
-    const usuario = this.usuarioService.getUsuarioLogado();
-    if (!usuario) return new Observable(subscriber => subscriber.next([]));
-
-    return runInInjectionContext(this.injetor, () => {
-      return this.firestore.collection<Tarefa>(this.NOME_COLECAO, ref =>
-        ref.where('usuarioID', '==', usuario.id)) // Apenas tarefas do usuário logado
-        .valueChanges({ idField: 'id' });
-    });
+  getTasks() : Observable<Tarefa[]> {
+    // Faz um GET para o json server
+    return this.http.get<Tarefa[]>(this.URL);
   }
 
   // Cria uma nova tarefa
-  addTask(tarefa: Tarefa): Observable<Tarefa> {
-    const usuario = this.usuarioService.getUsuarioLogado();
-    if (!usuario) return new Observable(subscriber => subscriber.next(tarefa));
-
-    tarefa.usuarioID = usuario.id!;
-
-    delete tarefa.id;
-    return from(this.colecaoTarefas.add({ ...tarefa })).pipe(
-      switchMap((docRef: DocumentReference<Tarefa>) => docRef.get()),
-      map(doc => ({ id: doc.id, ...doc.data() } as Tarefa))
-    );
+  addTask(tarefa: Tarefa) : Observable<Tarefa> {
+    return this.http.post<Tarefa>(`${this.URL}`, tarefa);
   }
 
   // Atualiza o status da tarefa
-  updateTask(tarefa: Tarefa): Observable<void> {
-    return runInInjectionContext(this.injetor, () => {
-      return from(this.colecaoTarefas.doc(tarefa.id).update({ ...tarefa }));
-    });
+  updateTask(tarefa: Tarefa): Observable<Tarefa> {
+    return this.http.put<Tarefa>(`${this.URL}/${tarefa.id}`, tarefa);
   }
 
   // Remove uma tarefa
-  deleteTask(id: string): Observable<void> {
-    return runInInjectionContext(this.injetor, () => {
-      return from(this.colecaoTarefas.doc(id).delete());
-    });
+  deleteTask(tarefa: Tarefa): Observable<Tarefa>{
+    return this.http.delete<Tarefa>(`${this.URL}/${tarefa.id}`);
   }
-
-  /*
-  // Filtra uma tarefa por título e categoria (case insensitive)
-  pesquisarPorFiltro(valor: string): Observable<Tarefa[]> {
-    valor = valor.toLowerCase(); // Converte a busca para minúsculas
-
-    return runInInjectionContext(this.injetor, () => {
-      return this.firestore.collection<Tarefa>(this.NOME_COLECAO, ref =>
-        ref.where('usuarioID', '==', this.usuarioService.getUsuarioLogado()?.id) // Filtra por usuário logado
-      ).valueChanges({ idField: 'id' }).pipe(
-        map(tarefas => tarefas.filter(tarefa =>
-          tarefa.titulo.toLowerCase().includes(valor) || // Ignora maiúsculas e minúsculas
-          tarefa.categoria.toLowerCase().includes(valor)
-        ))
-      );
-    });
-  } */
-
-  // Filtra uma tarefa por título e categoria
-  pesquisarPorFiltro(titulo: string, categoria: string): Observable<Tarefa[]> {
-    const usuarioID = this.usuarioService.getUsuarioLogado()?.id;
-    if (!usuarioID) return new Observable(subscriber => subscriber.next([]));
-
-    return runInInjectionContext(this.injetor, () => {
-      return this.firestore.collection<Tarefa>(this.NOME_COLECAO, ref =>
-        ref.where('usuarioID', '==', usuarioID)
-      ).valueChanges({ idField: 'id' }).pipe(
-        map(tarefas => tarefas.filter(tarefa =>
-          (titulo ? tarefa.titulo.toLowerCase().includes(titulo.toLowerCase()) : true) &&
-          (categoria ? tarefa.categoria === categoria : true)
-        ))
-      );
-    });
-  }
-
 }
 
-
+// ------------------ FIRESTORE -------------------
+// export class TaskService {
+//   private injetor = inject(Injector);
+//   private colecaoTarefas: AngularFirestoreCollection<Tarefa>;
+//   private NOME_COLECAO = 'tarefas';
+//
+//   constructor(private firestore: AngularFirestore, private usuarioService: UsuarioService) {
+//     this.colecaoTarefas = this.firestore.collection(this.NOME_COLECAO);
+//     runInInjectionContext(this.injetor, () => {
+//       this.colecaoTarefas = this.firestore.collection(this.NOME_COLECAO);
+//     });
+//   }
+//
+//   // Listagem das tarefas
+//   getTasks(): Observable<Tarefa[]> {
+//     const usuario = this.usuarioService.getUsuarioLogado();
+//     if (!usuario) return new Observable(subscriber => subscriber.next([]));
+//
+//     return runInInjectionContext(this.injetor, () => {
+//       return this.firestore.collection<Tarefa>(this.NOME_COLECAO, ref =>
+//         ref.where('usuarioID', '==', usuario.id)) // Apenas tarefas do usuário logado
+//         .valueChanges({ idField: 'id' });
+//     });
+//   }
+//
+//   // Cria uma nova tarefa
+//   addTask(tarefa: Tarefa): Observable<Tarefa> {
+//     const usuario = this.usuarioService.getUsuarioLogado();
+//     if (!usuario) return new Observable(subscriber => subscriber.next(tarefa));
+//
+//     tarefa.usuarioID = usuario.id!;
+//
+//     delete tarefa.id;
+//     return from(this.colecaoTarefas.add({ ...tarefa })).pipe(
+//       switchMap((docRef: DocumentReference<Tarefa>) => docRef.get()),
+//       map(doc => ({ id: doc.id, ...doc.data() } as Tarefa))
+//     );
+//   }
+//
+//   // Atualiza o status da tarefa
+//   updateTask(tarefa: Tarefa): Observable<void> {
+//     return runInInjectionContext(this.injetor, () => {
+//       return from(this.colecaoTarefas.doc(tarefa.id).update({ ...tarefa }));
+//     });
+//   }
+//
+//   // Remove uma tarefa
+//   deleteTask(id: string): Observable<void> {
+//     return runInInjectionContext(this.injetor, () => {
+//       return from(this.colecaoTarefas.doc(id).delete());
+//     });
+//   }
+//
+//   /*
+//
+//   TODO: PARA CORRIGIR (ESTAVA UTILIZANDO O FIRESTORE)
+//   // Filtra uma tarefa por título e categoria
+//   pesquisarPorFiltro(titulo: string, categoria: string): Observable<Tarefa[]> {
+//     const usuarioID = this.usuarioService.getUsuarioLogado()?.id;
+//     if (!usuarioID) return new Observable(subscriber => subscriber.next([]));
+//
+//     return runInInjectionContext(this.injetor, () => {
+//       return this.firestore.collection<Tarefa>(this.NOME_COLECAO, ref =>
+//         ref.where('usuarioID', '==', usuarioID)
+//       ).valueChanges({ idField: 'id' }).pipe(
+//         map(tarefas => tarefas.filter(tarefa =>
+//           (titulo ? tarefa.titulo.toLowerCase().includes(titulo.toLowerCase()) : true) &&
+//           (categoria ? tarefa.categoria === categoria : true)
+//         ))
+//       );
+//     });
+//   }
+// }
